002577e46874b7a798faaea8068c5206b317614f
Andrew <kezin636@gmail.com>
2024-04-17 00:13:26 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div class="flex justify-center items-center w-full h-screen">
+<slot />
+</div>  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<svelte:head>
+    <title>Join Cherry Team</title>
+</svelte:head>
+
+<div class="w-1/4 min-h-32 bg-zinc-800 rounded-lg overflow-y-hidden scale-down" style="min-width: 430px;">
+    <img src="/head.png" alt="head" />
+    <div class="p-4">
+        <small class="opacity-75 text-sm font-semibold">YOU'VE BEEN INVITED TO JOIN A SERVER</small>
+        <div class="flex gap-5 w-full mt-3 justify-between items-center">
+            <div class="flex gap-5 w-full">
+                <img alt="cherry" src="/cherry.webp" class="w-14 h-14 rounded-2xl" />
+                <div>
+                    <h2 class="text-base font-semibold">Cherry</h2>
+                    <div class="flex gap-5 w-full opacity-50 translate-y-2">
+                        <span class="flex items-center">
+                            <span class="text-green-400 text-5xl leading-3 -translate-y-px">•</span>
+                            <span class="leading-3 ms-1">
+                                72 Online
+                            </span>
+                        </span>
+                        <span class="flex items-center">
+                            <span class="text-5xl leading-3 -translate-y-px">•</span>
+                            <span class="leading-3 ms-1">
+                                174 Members
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <a href="https://youtu.be/kf5eUikyXYA?t=11" target="_blank" class="bg-green-700 text-white font-medium px-5 py-3 rounded">Join</a>
+        </div>
+    </div>
+</div>
